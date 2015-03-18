9975bf4d2a9006f6fd3b7a8d7d44b43b86311b2f
Dwayne Macgowan <dwaynemac@gmail.com>
2015-03-18 18:49:15 -0300

    manually add locales paths

--- lib/padma-assets.rb
+++ lib/padma-assets.rb
@@ -4,6 +4,8 @@ module Padma
   module Assets
     class Engine < Rails::Engine
       engine_name "padma-assets"
+      config.i18n.load_path += Dir[File.join(File.expand_path(File.dirname(__FILE__)), 'locales', '*.yml')]
+      
     end
   end
 end
