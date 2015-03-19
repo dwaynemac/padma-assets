202afc10761c3d655ca45b93c7a64d3930454dc2
Dwayne Macgowan <dwaynemac@gmail.com>
2015-03-19 16:50:51 -0300

    manually set views path

--- lib/padma-assets.rb
+++ lib/padma-assets.rb
@@ -5,7 +5,7 @@ module Padma
     class Engine < Rails::Engine
       engine_name "padma-assets"
       config.i18n.load_path += Dir[File.join(File.expand_path(File.dirname(__FILE__)), 'locales', '*.yml')]
-      
+      config.paths.app.views << Dir[File.join(File.expand_path(File.dirname(__FILE__)), '../app/views')] 
     end
   end
 end
