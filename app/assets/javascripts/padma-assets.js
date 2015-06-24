be245d5cdc17468cc37be62d212d561584b3d709
Dwayne Macgowan <dwaynemac@gmail.com>
2015-06-24 15:38:58 -0300

    explicit order of jss

--- app/assets/javascripts/padma-assets.js
+++ app/assets/javascripts/padma-assets.js
@@ -1,5 +1,14 @@
-//= require jquery.gritter.min
+//= require bootstrap.min
+//= require jquery-2.1.4.min
 //= require jquery.ba-throttle-debounce.min
+//= require jquery.gritter.min
+//= require jquery-iframe-transport
+//= require jquery-tmpl-min
+//= require jquery.postmessage-transport
+//= require jquery.rest_in_place
 //= require jquery.stickyheader
+//= require jquery.tipsy
+//= require jquery.xdr-transport
+//= require mixpanel
 //= require notifications
-//= require_tree 
+//= require ux-events
