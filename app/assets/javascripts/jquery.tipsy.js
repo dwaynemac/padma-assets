295e764fdbf8c1ca8ad849ceced0a0dabdeacc43
Dwayne Macgowan <dwaynemac@gmail.com>
2015-07-20 18:07:11 -0300

    change deprecated tipsy code

--- app/assets/javascripts/jquery.tipsy.js
+++ app/assets/javascripts/jquery.tipsy.js
@@ -166,10 +166,12 @@
         if (!options.live) this.each(function() { get(this); });
         
         if (options.trigger != 'manual') {
-            var binder   = options.live ? 'live' : 'bind',
-                eventIn  = options.trigger == 'hover' ? 'mouseenter' : 'focus',
-                eventOut = options.trigger == 'hover' ? 'mouseleave' : 'blur';
-            this[binder](eventIn, enter)[binder](eventOut, leave);
+          var eventIn = options.trigger == 'hover' ? 'mouseenter' : 'focus',
+            eventOut = options.trigger == 'hover' ? 'mouseleave' : 'blur';
+          if (options.live)
+            $(document).on(eventIn, this.selector, enter).on(eventOut, this.selector, leave);
+          else
+            this.bind(eventIn, enter).bind(eventOut, leave);
         }
         
         return this;
